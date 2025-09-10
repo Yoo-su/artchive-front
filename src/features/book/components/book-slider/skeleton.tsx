d317e7496e3245bb7368dbb44b921fe6024ee4f4
유수현 <rhan0871@naver.com>
2025-09-10 19:35:39 +0900

    feat: 메인페이지 최신 포스트 슬라이더 스켈레톤 높이, 헤더문구 등 수정

--- src/features/book/components/book-slider/skeleton.tsx
+++ src/features/book/components/book-slider/skeleton.tsx
@@ -27,17 +27,23 @@ export const BookSliderSkeleton = () => (
 
 export const RecentPostsSliderSkeleton = () => {
   return (
-    <div className="w-full overflow-hidden">
-      <div className="flex gap-8">
+    <div className="w-full overflow-hidden px-4">
+      <div className="flex gap-8 animate-pulse">
         {Array.from({ length: 5 }).map((_, i) => (
           <div
             key={i}
             className="flex flex-col items-center flex-shrink-0 w-40"
           >
-            <Skeleton className="w-40 h-40 rounded-full" />
-            <Skeleton className="h-5 w-3/4 mt-4" />
-            <Skeleton className="h-6 w-1/2 mt-2" />
-            <Skeleton className="h-4 w-2/3 mt-2" />
+            {/* 실제 카드 이미지 영역과 동일한 크기 및 여백 */}
+            <div className="w-40 h-40 mb-4">
+              <Skeleton className="w-full h-full rounded-full" />
+            </div>
+            {/* 실제 카드 텍스트 영역과 동일한 크기 및 여백 */}
+            <div className="w-full px-2 text-center">
+              <Skeleton className="h-5 w-3/4 mx-auto" />
+              <Skeleton className="h-7 w-1/2 mx-auto mt-1" />
+              <Skeleton className="h-4 w-2/3 mx-auto mt-2" />
+            </div>
           </div>
         ))}
       </div>
